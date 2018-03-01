0225a89e499b724b214e0371fac237650bc317cb
pautenavidal <pautenavidal@gmail.com>
2018-03-01 21:29:09 +0100

    solution finder done

---
+++ algorythm.py
@@ -0,0 +1,8 @@
+class Algorythm(object):
+
+    def __init__(self, initial_state):
+        self.state = initial_state
+
+    def calculate(self):
+
+        heuristic = self
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from algorythm import Algorythm
 from utils import read_file
 
 input = input("Enter input file (a, b, c, d):")
@@ -6,3 +7,6 @@ print("Selected input: ", input)
 
 file_path = '{0}_example.in'.format(input)
 initial_solution = read_file(file_path)
+
+algorythm = Algorythm(initial_solution)
+solution = algorythm.calculate()
